5b2d65341769e686d2e2927527315ffca0c2d492
dsingh44 <80799486+dsingh44@users.noreply.github.com>
2023-05-25 16:26:32 +0000

    Added petition section. User have option to input their info to support petition.

--- index.js
+++ index.js
@@ -8,3 +8,26 @@ const toggleDarkMode = () => {
 }
 // click event listener for theme button
 themeButton.addEventListener("click",toggleDarkMode);
+
+
+
+// query for the sign now button 
+const signNowButton = document.querySelector('#sign-now-button');
+
+const addSignature = () => {
+   //code to manipulate the DOM here
+  const name = document.querySelector('#name').value;
+  const hometown=document.querySelector('#hometown').value;
+
+  const signaturesContainer = document.querySelector(".signatures");
+  const newSignature = document.createElement("p");
+  newSignature.textContent = `🖊️ ${name} from ${hometown} support this`;
+  signaturesContainer.appendChild(newSignature);
+
+  // Clear the form inputs
+  document.querySelector('#name').value='';
+  document.querySelector('#hometown').value='';
+}
+
+// Add a click event listener to the sign now button here
+signNowButton.addEventListener("click",addSignature);
