b1ee6a70bd152b7b64e22ba07ce62ddaa0344856
dsingh44 <80799486+dsingh44@users.noreply.github.com>
2023-06-01 20:48:01 +0000

    Added scroll animation and modal that  thanks the user after their submission. Also updated styles.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let scaleFactor= 1;
+let modalImage = document.querySelector('.modal img');
 // Query for button with id theme-button
 let themeButton = document.getElementById("theme-button");
 
@@ -5,20 +7,21 @@ let themeButton = document.getElementById("theme-button");
 const toggleDarkMode = () => {
   document.body.classList.toggle("dark-mode");
 }
+
 // click event listener for theme button
 themeButton.addEventListener("click",toggleDarkMode);
-
 // query for the sign now button 
 const signNowButton = document.querySelector('#sign-now-button');
 
-const addSignature = () => {
+/*
+This function add signatures contatiners in Document Object Model
+*/
+const addSignature = (person) => {
   //code to manipulate the DOM 
-  const name = document.querySelector('#name').value;
   const hometown=document.querySelector('#hometown').value;
-
   const signaturesContainer = document.querySelector(".signatures");
   const newSignature = document.createElement("p");
-  newSignature.textContent = `🖊️ ${name} from ${hometown} support this`;
+  newSignature.textContent = `🖊️ ${person.name} from ${hometown} support this`;
   signaturesContainer.appendChild(newSignature);
 
   // Clear the form inputs
@@ -26,15 +29,16 @@ const addSignature = () => {
   document.querySelector('#hometown').value='';
 }
 
-// validation form function
+// This function form inputs and perform action based on result
 const validateForm =() => {
   let containsErrors = false;
-
   var petitionInputs = document.getElementById("sign-petition").elements;
-
+  let person = {
+    name:petitionInputs[0].value// acesses and save value of first input
+  }
+  
   //Loop through all inputs
   for(let i=0; i< petitionInputs.length;i++){
-
     if (petitionInputs[i].value.length<2){
       petitionInputs[i].classList.add('error');
       containsErrors =true;
@@ -43,14 +47,73 @@ const validateForm =() => {
       petitionInputs[i].classList.remove('error');
     }
   }
-  
   //call add signature and clear fields if no error
   if(containsErrors== false){
-    addSignature();
+    addSignature(person);
+    toggleModal(person);
     for (let index = 0; index < petitionInputs.length; index++) {
-      petitionInputs[i].value ="";
+      petitionInputs[index].value ="";
       containsErrors=false;
     }
   }
 }
 signNowButton.addEventListener('click',validateForm);
+// contain values for scrolling animation
+let animation ={
+  revealDistance: 150,
+  intialOpacity:0,
+  transitionDelay:0,
+  transitionDuration:'6s',
+  transitionProperty:'all',
+  transitionTimingFunction:'ease'
+}
+let revealableContainers = document.querySelectorAll('.revealable');
+
+//This function will contain a loop that check each revealable container to see if it is in the right position to be reavealed
+function reveal () {
+  for (i=0; i< revealableContainers.length; i++){
+    let windowHeight = window.innerHeight;
+    let topOfRevealableContainer = revealableContainers[i].getBoundingClientRect().top;
+    if (topOfRevealableContainer < windowHeight - animation.revealDistance){
+      /*add the active class to revealableContainer's classlist*/
+      revealableContainers[i].classList.add('active');
+    } else{
+      /* remove the active class to the revealableContainer's classslist */
+      revealableContainers[i].classList.remove('active');
+    }
+  }
+}
+window.addEventListener('scroll',reveal);
+
+//This function toggles a model and perform an animation on image element with the modal
+const toggleModal= (person) =>{
+  //thanks modal element
+  let modal = document.getElementById("thanks-modal");
+  //select thanks content modal element
+  let modalContent = document.getElementById("thanks-modal-content");
+  let intervalId;
+  modal.style.display="flex";
+  //set text content of modal with thank you message
+  modalContent.textContent = `Thank you ${person.name} for signing the petition!`;
+  
+  intervalId = setInterval(() => {
+    scaleImage();
+  },500);
+
+  //stop the animation 
+  setTimeout(() => {
+    clearInterval(intervalId);
+    modal.style.display ="none";
+  }, 4000);  
+}
+//This function scales the image dispalyed in the modal
+const scaleImage=() =>{
+  if (scaleFactor == 1){
+    scaleFactor= 0.8;
+  }
+  else{
+    scaleFactor =1;
+  }
+  //apply the scale factor to image
+  modalImage.style.transform =`scale(${scaleFactor})`;
+}
